3041e609bbd792c8e4101fb10fe1c9eff6e9a82a
RedmarAlkema <rag.alkema@student.avans.nl>
2024-08-08 21:29:27 +0200

    werkende opzet website

--- goudendraak/routes/web.php
+++ goudendraak/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/paginas/aanbiedingen', function () {
+    return view('paginas.aanbiedingen');
+})->name('aanbiedingen');
+
+Route::get('/paginas/MENUKAART', function () {
+    return view('paginas.MENUKAART');
+})->name('menukaart');
+
+Route::get('/paginas/news', function () {
+    return view('paginas.news');
+})->name('news');
+
+Route::get('/paginas/contact', function () {
+    return view('paginas.contact');
+})->name('contact');
+
+Route::get('/paginas/contact_new', function () {
+    return view('paginas.contact_new');
+})->name('contact_new');
